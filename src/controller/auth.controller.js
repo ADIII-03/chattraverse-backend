80c547f763ffa146b5b43502975646722a9144bf
Aditya Raman <aradityaraman0518@gmail.com>
2025-06-01 00:13:43 +0530

    Update auth.controller.js

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -76,7 +76,7 @@ export async  function login(req,res){
             maxAge:7*24*60*60*1000,
             httpOnly:true,
             secure:process.env.NODE_ENV === "production",
-            sameSite:"strict"});
+            sameSite:"None"});
         res.status(200).json({ success : true ,user ,});
                 
            
