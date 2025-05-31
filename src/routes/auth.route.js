51ba48c056c40e12a1a487fc34728734550661b5
ADIII-03 <aradityaraman0518@gmail.com>
2025-05-31 23:21:32 +0530

    g

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,6 +4,13 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router()
 
+// This route handles user signup
+
+router.get("/me", protectRoute, (req, res) => {
+  // This route is used to get the authenticated user's details
+  res.status(200).json({ user: req.user });
+});
+
 router.post("/signup",signup);
 
 
